351bd1d203266b85470fb9dccde007b2711f2599
starsserver44 <43623403+starsserver44@users.noreply.github.com>
2018-09-27 00:57:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -208,31 +208,4 @@ function isYoutube(str) {
       message.channel.send({embed});
      }
     });
- 
-const adminprefix = "1vip";
-const devs = ['337457211875917834' , '365929628226945028' , '365929628226945028'];
-client.on('message', message => {//for dev
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setavt')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/sytraaa");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
-
-
-    });
  client.login(process.env.BOT_TOKEN);
